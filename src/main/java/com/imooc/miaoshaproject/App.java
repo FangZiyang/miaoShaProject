ee2b3ac245a2ba8b804a80a8b97db5769c7ae71e
fangziyang <657422479@qq.com>
2021-08-25 15:56:51 +0800

    com imcoom 傻傻的分不清

---
+++ src/main/java/com/imooc/miaoshaproject/App.java
@@ -0,0 +1,44 @@
+package com.imooc.miaoshaproject;
+
+import com.imooc.miaoshaproject.dao.UserDOMapper;
+import com.imooc.miaoshaproject.dao.UserPasswordDOMapper;
+import com.imooc.miaoshaproject.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ */
+
+@SpringBootApplication(scanBasePackages = {"com.imooc.miaoshaproject"})
+@RestController
+@MapperScan("com.imooc.miaoshaproject.dao")
+public class App {
+
+    @Autowired
+    private UserDOMapper userDOMapper;
+
+//    @Autowired
+//    private UserPasswordDOMapper userPasswordDOMapper;
+
+    @RequestMapping("/")
+    public String home() {
+        System.out.println("huan ying hui jia");
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if (userDO == null) {
+            return "user not found";
+        } else {
+            return userDO.getName();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args);
+    }
+}
