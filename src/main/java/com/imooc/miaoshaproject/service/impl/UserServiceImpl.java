206d3d81bac8e56f268bfa98c03273c44e47f09a
fangziyang <657422479@qq.com>
2021-09-03 11:26:20 +0800

    判断方式进行优化

--- src/main/java/com/imooc/miaoshaproject/service/impl/UserServiceImpl.java
+++ src/main/java/com/imooc/miaoshaproject/service/impl/UserServiceImpl.java
@@ -8,14 +8,15 @@ import com.imooc.miaoshaproject.error.BusinessException;
 import com.imooc.miaoshaproject.error.EmBusinessError;
 import com.imooc.miaoshaproject.service.UserService;
 import com.imooc.miaoshaproject.service.model.UserModel;
-import org.apache.catalina.User;
+import com.imooc.miaoshaproject.validator.ValidationResult;
+import com.imooc.miaoshaproject.validator.ValidatorImpl;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.jws.soap.SOAPBinding;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -25,6 +26,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     UserPasswordDOMapper userPasswordDOMapper;
+
+    @Autowired
+    private ValidatorImpl validator;
 
     @Override
     public UserModel getUserById(Integer id) {
@@ -55,19 +59,55 @@ public class UserServiceImpl implements UserService {
         if (result.isHasErrors()) {
             throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, result.getErrMsg());
         }
+
+        UserDO userDO = convertFromModel(userModel);
+        try {
+            userDOMapper.insertSelective(userDO);
+        } catch (DuplicateKeyException ex) {
+            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, "手机号已重复注册");
+        }
+
+
+        userModel.setId(userDO.getId());
+
+        UserPasswordDO userPasswordDO = convertPasswordFromModel(userModel);
+        userPasswordDOMapper.insertSelective(userPasswordDO);
+
+        return;
     }
 
     @Override
     public UserModel validateLogin(String telephone, String encrptPassword) throws BusinessException {
         UserDO userDO = userDOMapper.selectByTelephone(telephone);
-        if(userDO == null){
+        if (userDO == null) {
             throw new BusinessException(EmBusinessError.USER_LOGIN_FAIL);
         }
         UserPasswordDO userPasswordDO = userPasswordDOMapper.selectByUserId(userDO.getId());
-        UserModel userModel = convertFromDataObject(userDO,userPasswordDO);
-        if(!StringUtils.equals(encrptPassword,userModel.getEncrptPassword())){
+        UserModel userModel = convertFromDataObject(userDO, userPasswordDO);
+        if (!StringUtils.equals(encrptPassword, userModel.getEncrptPassword())) {
             throw new BusinessException(EmBusinessError.USER_LOGIN_FAIL);
         }
         return userModel;
     }
+
+    private UserDO convertFromModel(UserModel userModel) {
+        if (userModel == null) {
+            return null;
+        }
+        UserDO userDO = new UserDO();
+        BeanUtils.copyProperties(userModel, userDO);
+
+        return userDO;
+    }
+
+    private UserPasswordDO convertPasswordFromModel(UserModel userModel) {
+        if (userModel == null) {
+            return null;
+        }
+        UserPasswordDO userPasswordDO = new UserPasswordDO();
+        userPasswordDO.setEncrptPassword(userModel.getEncrptPassword());
+        userPasswordDO.setUserId(userModel.getId());
+        return userPasswordDO;
+    }
+
 }
